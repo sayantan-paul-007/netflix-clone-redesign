b8a527e2fcb05ae4e4f10d7605c6debe56650bbc
Sayantan Paul <sayantanpaul222@gmail.com>
2023-01-11 01:02:05 +0530

    netflix

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('#one').click(function () { 
+     $('#oneplus').toggleClass('rotateplus');
+     $('#one-sub').slideToggle();
+    });
+    $('#two').click(function () { 
+        $('#twoplus').toggleClass('rotateplus');
+        $('#two-sub').slideToggle();
+       });
+       $('#three').click(function () { 
+        $('#threeplus').toggleClass('rotateplus');
+        $('#three-sub').slideToggle();
+       });
+       $('#four').click(function () { 
+        $('#fourplus').toggleClass('rotateplus');
+        $('#four-sub').slideToggle();
+       });
+       $('#five').click(function () { 
+        $('#fiveplus').toggleClass('rotateplus');
+        $('#five-sub').slideToggle();
+       });
+       $('#six').click(function () { 
+        $('#sixplus').toggleClass('rotateplus');
+        $('#six-sub').slideToggle();
+       });
+});
